decfa318206bddde962fadc423e1ba61e10db24c
Thomas Savary <savarythomas2102@gmail.com>
2024-04-25 11:49:10 +0200

    Modify evaluate

--- evaluate.py
+++ evaluate.py
@@ -5,6 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
+# Parameters
+seq_length = 200
+
 # Load model
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = torch.load('./trainedModel.pt').to(device)
@@ -14,8 +17,8 @@ x_val, y_val = np.load('./Data/x_test.npy'), np.load('./Data/y_test.npy')
 x_val = torch.tensor(x_val, dtype = torch.float32, device = device)
 
 # Use the model to make prediction
-logits = model(x_val).cpu()
-probs = torch.nn.functional.softmax(logits, dim = -1)
+logits = model(x_val)
+probs = torch.nn.functional.softmax(logits, dim = -1).cpu()
 y_hat = torch.argmax(probs, dim = -1).numpy()
 
 # Compute accuracy
@@ -31,12 +34,32 @@ plt.ylabel('True Label')
 plt.title('Confusion Matrix')
 plt.show()
 
-# Load and plot trajectory
-traj = np.load('./CLVs/testTraj_1.npy')
+# Load trajectory, compute the angles and make predictions
+traj, CLV_ = np.load('./CLVs/testTraj_1.npy'), np.load('./CLVs/testCLV_1.npy')
+n, dim, _ = CLV_.shape
+
+angles = np.zeros((n, dim))
+for j in range(n):
+    angles[j,0] = np.arccos(np.dot(CLV_[j,:,0], CLV_[j,:,1]))
+    angles[j,1] = np.arccos(np.dot(CLV_[j,:,0], CLV_[j,:,2]))
+    angles[j,2] = np.arccos(np.dot(CLV_[j,:,1], CLV_[j,:,2]))
+
+predictions = []
+new_traj = []
+for j in range(seq_length, n):
+    x = angles[(j-seq_length):j, :]
+    x = torch.tensor(x, dtype = torch.float32, device = device).unsqueeze(0)
+    probs = torch.nn.functional.softmax(model(x), dim = -1).cpu()
+    predictions.append(torch.argmax(probs, dim = -1).numpy()[0])
+    new_traj.append(traj[j,:])
+predictions = np.asarray(predictions)
+new_traj = np.asarray(new_traj)
+
+# Plot predictions
 plt.figure()
-plt.plot(traj[199:199 + 1000,0], color = "blue", label = r'$x_{1}$')
-plt.plot(traj[199:199 + 1000,1], color = "black", label = r'$x_{2}$')
-plt.plot(5*y_hat[:801], color = "red", label = "Predictions")
+plt.plot(new_traj[:,0], color = "blue", label = r'$x_{1}$')
+plt.plot(new_traj[:,1], color = "black", label = r'$x_{2}$')
+plt.plot(5*predictions, color = "red", label = "Predictions")
 plt.xlabel(r'$t$')
 plt.legend()
 plt.show()
